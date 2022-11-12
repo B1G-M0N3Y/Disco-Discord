d85beba96ab0d95381fdf4af4e44297a5c14c5fe
B1G-M0N3Y <noahkerner843@gmail.com>
2022-11-12 16:27:32 -0700

    navbar changes when user is logged in or out

--- practice-for-week-19-python-project-skeleton/react-app/src/components/NavBar/NavBar.js
+++ practice-for-week-19-python-project-skeleton/react-app/src/components/NavBar/NavBar.js
@@ -1,37 +1,65 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import LogoutButton from "../auth/LogoutButton";
 import ChannelList from "../Channels/ChannelList";
-import './NavBar.css'
+import "./NavBar.css";
 
 const NavBar = () => {
+  const sessionUser = useSelector((state) => state.session.user);
+
+  let userDisplay;
+
+  if (sessionUser) {
+    userDisplay = (
+      <>
+        <div className="session-user-info">
+          <img src={sessionUser.image_url}></img>
+          <p>{sessionUser.username}</p>
+        </div>
+        <LogoutButton />
+      </>
+    );
+  } else {
+    userDisplay = (
+      <>
+        <NavLink to="/login" exact={true} activeClassName="active">
+          Login
+        </NavLink>
+        <NavLink to="/sign-up" exact={true} activeClassName="active">
+          Sign Up
+        </NavLink>
+      </>
+    );
+  }
+
   return (
     <nav>
       <ul className="navbar">
         <ChannelList />
         <li>
-          <NavLink className="navlink" to="/home" exact={true} activeClassName="active">
-            Home
+          {/* TODO: Insert logo here */}
+          <NavLink
+            className="navlink"
+            to="/home"
+            exact={true}
+            activeClassName="active"
+          >
+            LOGO HERE
           </NavLink>
         </li>
+
         <li>
-          <NavLink className="navlink" to="/login" exact={true} activeClassName="active">
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="navlink" to="/sign-up" exact={true} activeClassName="active">
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="navlink" to="/users" exact={true} activeClassName="active">
+          <NavLink
+            className="navlink"
+            to="/users"
+            exact={true}
+            activeClassName="active"
+          >
             Users
           </NavLink>
         </li>
-        <li>
-          <LogoutButton />
-        </li>
+        {userDisplay}
       </ul>
     </nav>
   );
