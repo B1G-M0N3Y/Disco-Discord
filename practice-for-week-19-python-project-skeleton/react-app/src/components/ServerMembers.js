1ff71787670c26f8f16657324658972e12825a57
Joanna <gilbert.joannac@gmail.com>
2022-11-13 16:05:15 -0700

    display server member list

--- practice-for-week-19-python-project-skeleton/react-app/src/components/ServerMembers.js
+++ practice-for-week-19-python-project-skeleton/react-app/src/components/ServerMembers.js
@@ -1,26 +1,21 @@
 import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch} from "react-redux";
 import { useParams } from "react-router-dom";
 import { NavLink } from "react-router-dom";
+import { getServerMembers } from "../store/servers";
 
 function ServerMembers() {
-  // get the server id from the url
   const { serverId } = useParams();
+  const dispatch = useDispatch();
+  const membersObj = useSelector((state) => state.servers.members);
+  const membersArr = Object.values(membersObj)
 
-  // getters and setters for members and users
-  const [members, setMembers] = useState([]);
-  const [users, setUsers] = useState([]);
-
-  // fetch the server members
+  // get members
   useEffect(() => {
-    async function fetchData() {
-      const response = await fetch("/api/servers/1/members")
-        .then((resp) => resp.json())
-        .then((data) => setMembers(data));
-    }
-    fetchData();
-  }, []);
+    dispatch(getServerMembers(serverId));
+  }, [dispatch]); 
 
-  const serverMembers = members.map((member) => {
+  const serverMembers = membersArr.map((member) => {
     return (
       <li key={member.user_id}>
         <NavLink to={`/users/${member.user_id}`}>{member.user_id}</NavLink>
@@ -30,8 +25,8 @@ function ServerMembers() {
 
   return (
     <>
-      <h1>User List: </h1>
-      <ul>{userComponents}</ul>
+      <h1>Member List: </h1>
+      <ul>{serverMembers}</ul>
     </>
   );
 }
