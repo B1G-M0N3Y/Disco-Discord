b4fe6c17046f041193bf9c45452c85242362009a
B1G-M0N3Y <noahkerner843@gmail.com>
2022-11-14 09:27:57 -0700

    merged dev with branch

--- practice-for-week-19-python-project-skeleton/react-app/src/App.js
+++ practice-for-week-19-python-project-skeleton/react-app/src/App.js
@@ -4,7 +4,7 @@ import { useDispatch } from "react-redux";
 import LoginForm from "./components/auth/LoginForm";
 import SignUpForm from "./components/auth/SignUpForm";
 import LandingPage from "./components/LandingPage";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar/NavBar";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import UsersList from "./components/UsersList";
 import ServerMembers from "./components/ServerMembers";
