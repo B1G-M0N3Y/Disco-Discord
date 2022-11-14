1740eff9a96aa68bfdf9989dc7bd5c1e84fc05a2
Joanna <gilbert.joannac@gmail.com>
2022-11-13 19:01:09 -0700

    display usernames

--- practice-for-week-19-python-project-skeleton/react-app/src/components/ServerMembers.js
+++ practice-for-week-19-python-project-skeleton/react-app/src/components/ServerMembers.js
@@ -7,18 +7,40 @@ import { getServerMembers } from "../store/servers";
 function ServerMembers() {
   const { serverId } = useParams();
   const dispatch = useDispatch();
+
+  // getters and setters
+  const [users, setUsers] = useState([]);
+
+  // get server member state
   const membersObj = useSelector((state) => state.servers.members);
   const membersArr = Object.values(membersObj)
 
-  // get members
+  // get server members with id from url
   useEffect(() => {
     dispatch(getServerMembers(serverId));
   }, [dispatch, serverId]); 
 
+  //
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("/api/users/");
+      const responseData = await response.json();
+      setUsers(responseData.users);
+    }
+    fetchData();
+  }, []);
+  
+  // find user from member's user_id
+  const findUser = (id) => {
+    const user = users.find((user) => user.id === id);
+    console.log(users)
+    return `${user?.username}`;
+  };
+
   const serverMembers = membersArr.map((member) => {
     return (
       <li key={member.user_id}>
-        <NavLink to={`/users/${member.user_id}`}>{member.user_id}</NavLink>
+        <NavLink to={`/users/${member.user_id}`}>{findUser(member.user_id)}</NavLink>
       </li>
     );
   });
