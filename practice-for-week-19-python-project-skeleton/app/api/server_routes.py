7a1ebb8c57fa84260602ab25dccdb9f18c7711e6
Joanna <gilbert.joannac@gmail.com>
2022-11-15 09:04:17 -0700

    refactor server.py model classes

--- practice-for-week-19-python-project-skeleton/app/api/server_routes.py
+++ practice-for-week-19-python-project-skeleton/app/api/server_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, jsonify, request
 from ..forms import ChannelForm, ServerForm, EditServerForm, AddServerMember
-from app.models.servers import db, ServerMember, Channel, Server, channel_schema, channels_schema, server_schema, servers_schema, server_members_schema
+from app.models.servers import db, server_members, Channel, Server, channel_schema, channels_schema, server_schema, servers_schema, server_members_schema
 
 server_routes = Blueprint('servers', __name__)
 
@@ -14,23 +14,33 @@ def validation_errors_to_error_messages(validation_errors):
             errorMessages.append(f'{field} : {error}')
     return errorMessages
 
-@server_routes.route('/<int:server_id>/channels', methods=["POST"])
-def post_new_channel(server_id):
-    """Create a new channel"""
-    form = ChannelForm()
-    server = Server.query.get(server_id)
-    form['csrf_token'].data = request.cookies['csrf_token']
-    if server and form.validate_on_submit():
-        data = form.data
-        new_channel = Channel(
-            name = data['name'],
-            server_id = server_id
-        )
-        db.session.add(new_channel)
-        db.session.commit()
-        result = channel_schema.dump(new_channel)
-        return (jsonify(result))
-    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+# @server_routes.route('/<int:server_id>/channels', methods=["POST"])
+# def create_channel(server_id):
+#     """
+#     Create a new channel
+#     """
+#     form = ChannelForm()
+#     server = Server.query.get(server_id)
+#     form['csrf_token'].data = request.cookies['csrf_token']
+#     if server and form.validate_on_submit():
+#         data = form.data
+#         chat_to = data['channel_members']
+#         new_channel = Channel(
+#             name = data['name'],
+#             server_id = server_id
+#         )
+#         channel_members = [int(channel_member)
+#                         for channel_member in data["channel_members_lst"].split(",")]
+#         for channel_member in channel_members:
+#             channel_user = User.query.get(channel_member)
+#             new_channel.channel_members.append(channel_user)        
+
+#         db.session.add(new_channel)
+#         db.session.commit()
+
+#         result = Channel.query.order_by(Channel.id.desc()).first()
+#         return (jsonify(channel_schema.dump(result))
+    # return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 @server_routes.route('/<int:server_id>/channels', methods=["GET"])
 def get_all_channels(server_id):
--- practice-for-week-19-python-project-skeleton/app/models/servers.py
+++ practice-for-week-19-python-project-skeleton/app/models/servers.py
@@ -1,21 +1,34 @@
 from sqlalchemy import func
 from sqlalchemy.orm import validates
-from .db import db, ma, environment, SCHEMA
+from .db import db, ma, environment
+
+# Join Table For Users & Server Members (Many to Many)
+server_members = db.Table(
+    "server_members",
+    db.Column("server_id",
+              db.Integer(),
+              db.ForeignKey('servers.id'),
+              primary_key=True),
+    db.Column("user_id",
+              db.Integer(),
+              db.ForeignKey('users.id'),
+              primary_key=True)
+)
 
 
 class Server(db.Model):
     __tablename__ = "servers"
 
-    if environment == "production":
-        __table_args__ = {'schema': SCHEMA}
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     image_url = db.Column(db.String(255))
-    admin_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    admin_id = db.Column(db.Integer(), db.ForeignKey(
+        'users.id'), nullable=False)
 
+    server_members = db.relationship(
+        "User", secondary=server_members, back_populates="servers")    
     channels = db.relationship("Channel", back_populates="server")
-    users = db.relationship("User", back_populates="servers")
+    # users = db.relationship("User", back_populates="servers")
 
     def to_dict(self):
         return {
@@ -25,6 +38,31 @@ class Server(db.Model):
             'image_url': self.image_url
         }
 
+class ChannelMessages(db.Model):
+    __tablename__ = "channel_messages"
+
+    id = db.Column(db.Integer, primary_key=True)
+    channel_id = db.Column(db.Integer, db.ForeignKey("channels.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    body = db.Column(db.String(2000), nullable=False)
+    # createdAt = db.Column(db.DateTime(), nullable=False)
+    # updatedAt = db.Column(db.DateTime())
+    created_at = db.Column(db.DateTime(timezone=True),
+                           server_default=func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
+
+    # many to one (many chatmessages to one user/author)
+    message_author = db.relationship("User", back_populates="server_messages")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "channel_id": self.channel_id,
+            "user_id": self.user_id,
+            "body": self.body, 
+            "createdAt": self.createdAt, 
+            "updatedAt": self.updatedAt
+        }
 
 class Channel(db.Model):
     __tablename__ = "channels"
@@ -34,37 +72,23 @@ class Channel(db.Model):
     server_id = db.Column(db.Integer, db.ForeignKey(
         "servers.id"), nullable=False)
 
-    messages = db.relationship("ChannelMessages")
+    # messages = db.relationship("ChannelMessages")
     server = db.relationship("Server", back_populates="channels")
 
     def to_dict(self):
         return {
-            id: self.id,
-            name: self.name,
-            server_id: self.server_id
+            "id": self.id,
+            "name": self.name,
+            "server_id": self.server_id
         }
 
 
-class ServerMember(db.Model):
-    __tablename__ = "server_members"
+# class ServerMember(db.Model):
+#     __tablename__ = "server_members"
 
-    id = db.Column(db.Integer, primary_key=True)
-    server_id = db.Column("server_id", db.Integer, db.ForeignKey("servers.id"))
-    user_id = db.Column("user_id", db.Integer, db.ForeignKey("users.id"))
-
-
-class ChannelMessages(db.Model):
-    __tablename__ = "channel_messages"
-
-    id = db.Column(db.Integer, primary_key=True)
-    channel_id = db.Column(db.Integer, db.ForeignKey("channels.id"))
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    body = db.Column(db.String(2000), nullable=False)
-    created_at = db.Column(db.DateTime(timezone=True),
-                           server_default=func.now())
-    updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
-
-    # channel = db.relationship("Channel")
+#     id = db.Column(db.Integer, primary_key=True)
+#     server_id = db.Column("server_id", db.Integer, db.ForeignKey("servers.id"))
+#     user_id = db.Column("user_id", db.Integer, db.ForeignKey("users.id"))
 
 
 class ServerSchema(ma.Schema):
