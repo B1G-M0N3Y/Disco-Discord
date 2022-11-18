d513b861ecb9a7a14293fd22bc7c400712290abf
Joanna <gilbert.joannac@gmail.com>
2022-11-17 17:52:12 -0700

    fixed bugs

--- practice-for-week-19-python-project-skeleton/react-app/src/store/servers.js
+++ practice-for-week-19-python-project-skeleton/react-app/src/store/servers.js
@@ -122,7 +122,7 @@ export const removeServerMember = (serverId) => async (dispatch) => {
     method: "DELETE",
   });
   if (response.ok) {
-    dispatch(remove(serverId));
+    dispatch(deleteServerAction(serverId));
   }
 };
 
