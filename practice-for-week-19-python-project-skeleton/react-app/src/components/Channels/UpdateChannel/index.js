df0adb84ee72575d103cac4583245f8a294b2bcb
Kristopher Han <kris@kristopherhan.com>
2022-11-18 12:14:24 -0800

    Halfway through update Channel Component

--- practice-for-week-19-python-project-skeleton/react-app/src/components/Channels/UpdateChannel/index.js
+++ practice-for-week-19-python-project-skeleton/react-app/src/components/Channels/UpdateChannel/index.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 import { updateChannelThunk, getCurrentChannels, deleteChannel} from "../../../store/channels"
+import { useSelectedChannels } from "../../../context/ChannelContext";
 
 const UpdateChannel = ({channel})
     const dispatch = useDispatch();
@@ -10,14 +11,44 @@ const UpdateChannel = ({channel})
     const user = useSelector((state) => state.session.user);
     const userId = useSelector((state) => state.session.user.id);
     const servers = useSelector((state) => state.servers.servers);
-    const serverArr = Object.values(servers);
+    const channels = useSelector((state) => state.servers.channels);
+    const serversArr = Object.values(servers);
+    const channelsArr = Object.values(channels);
+
 
     // identify the server that matches the id from url
-    const thisServer = serverArr.find((item) => item.id === server?.id);
+    const thisChannel = channelsArr.find((item) => item.id === channel?.id);
 
     // getters and setters for update channel form
     const [name, setName] = useState(channel?.name);
-    const [serverId, setServerId] = useState(server?.image_url);
+    const [channelId, setChannelId] = useState(channel?.id);
 
     const [validationErrors, setValidationErrors] = useState([]);
-    const { selectedServer, setSelectedServer } = useSelectedServer();
+    const { selectedChannel, setSelectedChannel } = useSelectedChannels();
+
+    useEffect(() => {
+        dispatch(getCurrentChannels(selectedChannel.id));
+    })
+
+    useEffect(() => {
+        const errors = [];
+
+        if (!name) errors.push("New Channel Name required for Update.")
+        setValidationErrors(errors);
+
+        if (!channelId) errors.push("No Channel found. Please check your Server.")
+    }, [name, channelId])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        let updateChannelInputs;
+
+        if (validationErrors.length > 0)
+            updateChannelInputs = {
+                name: name,
+                channel_Id: channelId
+            };
+
+        const updatedChannel = await dispatch(updateChannelThunk(updateChannelInputs, selectedChannel?.id));
+    }
