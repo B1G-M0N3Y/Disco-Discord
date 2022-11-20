a9866ebe40ea1b93f5d3d19e895ac1918fc1397d
Joanna <gilbert.joannac@gmail.com>
2022-11-19 20:10:21 -0700

    bugs

--- practice-for-week-19-python-project-skeleton/react-app/src/components/ServerMembers/NewServerMember.js
+++ practice-for-week-19-python-project-skeleton/react-app/src/components/ServerMembers/NewServerMember.js
@@ -1,3 +1,190 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import {
+  getAllUsers,
+  getMembers,
+  setServerUsers,
+  addServerMember,
+  removeServerMember,
+} from "../../store/users";
+import { getServers } from "../../store/servers";
+import { useSelectedServer } from "../../context/ServerContext";
+
+const NewServerMember = ({ serverId, currMembers }) => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.session.user);
+  const servers = useSelector((state) => state.servers.servers);
+  const allUsers = useSelector((state) => state.users.allUsers);
+  const serverUsers = useSelector((state) => state.users.serverUsers);
+  const { selectedServer } = useSelectedServer();
+  const serversArr = Object.values(servers);
+  let membersArr = Object.values(serverUsers);
+  const usersArr = Object.values(allUsers)[0];
+  const currServer = servers[serverId];
+
+  // let currMembers = [];
+  const [members, setMembers] = useState([]);
+  const [userId, setUserId] = useState();
+  const [memberId, setMemberId] = useState();
+  const [userSelect, setUserSelect] = useState([]);
+  const [memberSelect, setMemberSelect] = useState([]);
+  const [validationErrors, setValidationErrors] = useState([]);
+
+  if (currServer?.server_members) {
+    currMembers = Object.values(currServer?.server_members);
+  }
+
+  useEffect(() => {
+    dispatch(getServers());
+    dispatch(getAllUsers());
+    dispatch(setServerUsers(currMembers));
+  }, [serverId]);
+
+  useEffect(() => {
+    let selected = usersArr?.find((user) => user.username === memberSelect);
+    console.log("MEMBER ID SELECTED", memberId);
+    setMemberId(selected?.id);
+  }, [memberSelect]);
+
+  useEffect(() => {
+    let selected = usersArr?.find((user) => user.username === userSelect);
+    console.log("USER ID SELECTED", userId);
+    setUserId(selected?.id);
+  }, [userSelect]);
+
+  const revert = () => {
+    setUserId();
+    setUserSelect([]);
+    setMemberId();
+    setMemberSelect([]);
+  };
+
+  // form validations
+  useEffect(() => {
+    const errors = [];
+    setValidationErrors(errors);
+    // if (!name) errors.push("Server name is required.");
+    setValidationErrors(errors);
+  }, []);
+
+  const handleAdd = async (e) => {
+    e.preventDefault();
+    setValidationErrors([]);
+    // a user needs to be the admin in order to allow editing
+    // if (user.id === serversArr[serverId]?.admin_id) {
+    await dispatch(addServerMember(serverId, userId));
+    return history.push(`/servers/${serverId}`);
+    // }
+  };
+
+  const handleRemove = async (e) => {
+    e.preventDefault();
+    setValidationErrors([]);
+    // a user needs to be the admin in order to allow editing
+    // if (user.id === serversArr[serverId]?.admin_id) {
+    dispatch(removeServerMember(serverId, memberId));
+    dispatch(getMembers());
+    revert();
+    return history.push(`/servers/${serverId}`);
+    // }
+  };
+
+  let notJoined;
+  let currMembersId = currMembers.map((member) => member.id);
+
+  if (allUsers) {
+    notJoined = Object.values(allUsers)[0]?.filter(
+      (user) => !currMembersId.includes(user.id)
+    );
+  }
+
+  console.log(usersArr, "USERS");
+  console.log(membersArr, "MEMBERS");
+  console.log(notJoined, "NOT MEMBERS");
+
+  // set user albums for form select
+  const updateMember = (e) => {
+    setMemberSelect(e.target.value);
+    console.log("member select ", memberSelect);
+  };
+
+  const updateUser = (e) => {
+    setUserSelect(e.target.value);
+    console.log("member select ", userSelect);
+  };
+
+  return (
+    <>
+      <form className="add-remove-form" onSubmit={handleAdd}>
+        <div className="add-members">Add A User:</div>
+        <select
+          onChange={updateUser}
+          value={userSelect}
+          placeholder="Add a user:"
+        >
+          <option value="" disabled selected>
+            Select an user to add...
+          </option>
+          {notJoined?.map((user) => {
+            return <option key={user.username}>{user.username}</option>;
+          })}
+        </select>
+        <ul className="errors">
+          {validationErrors.length > 0 &&
+            validationErrors.map((err) => (
+              <li id="err" key={err}>
+                {err}
+              </li>
+            ))}
+        </ul>
+        <button
+          className="edit-server-submit"
+          type="submit"
+          disabled={!!validationErrors.length}
+        >
+          Add
+        </button>
+      </form>
+
+      <form className="remove-form" onSubmit={handleRemove}>
+        <div className="remove-members">Remove A Member:</div>
+        <select
+          onChange={updateMember}
+          value={memberSelect}
+          placeholder="Remove a member:"
+        >
+          <option value="" disabled selected>
+            Select an member to remove...
+          </option>
+          {membersArr?.map((user) => {
+            return <option key={user.username}>{user.username}</option>;
+          })}
+        </select>
+        <ul className="errors">
+          {validationErrors.length > 0 &&
+            validationErrors.map((err) => (
+              <li id="err" key={err}>
+                {err}
+              </li>
+            ))}
+        </ul>
+        <button
+          className="edit-server-submit"
+          type="submit"
+          disabled={!!validationErrors.length}
+        >
+          Remove
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default NewServerMember;
+
+/*
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllUsers } from "../../store/users";
@@ -9,7 +196,7 @@ const NewServerMember = ({ serverId, currMembers }) => {
   const addMember = async (userId) => {
     console.log("user id", userId);
     console.log("server id", serverId);
-    const response = await fetch(`/api/servers/${serverId}/members`, {
+    const response = await fetch(/api/servers/${serverId}/members, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -57,3 +244,4 @@ const NewServerMember = ({ serverId, currMembers }) => {
 };
 
 export default NewServerMember;
+*/
--- practice-for-week-19-python-project-skeleton/react-app/src/store/users.js
+++ practice-for-week-19-python-project-skeleton/react-app/src/store/users.js
@@ -1,56 +1,116 @@
-const GET_USERS = 'users/GET_USERS'
-const REMOVE_MEMBER = 'users/REMOVE_MEMBER'
-const SET_USERS = 'users/SET_USERS'
+const GET_USERS = "users/GET_USERS";
+const GET_MEMBERS = "users/GET_MEMBERS";
+const ADD_MEMBER = "users/ADD_MEMBER";
+const REMOVE_MEMBER = "users/REMOVE_MEMBER";
+const SET_USERS = "users/SET_USERS";
 
 const getUsers = (users) => ({
-    type: GET_USERS,
-    users
-})
+  type: GET_USERS,
+  users,
+});
 
-const removeMember = (memberId) => ({
-    type: REMOVE_MEMBER,
-    memberId
-})
+export const getMembers = (members) => ({
+  type: GET_MEMBERS,
+  members,
+});
+
+const addMember = (user) => ({
+  type: ADD_MEMBER,
+  user,
+});
+
+const removeMember = (userId) => ({
+  type: REMOVE_MEMBER,
+  userId,
+});
 
 export const setServerUsers = (users) => ({
-    type: SET_USERS,
-    users
-})
+  type: SET_USERS,
+  users,
+});
 
 export const getAllUsers = () => async (dispatch) => {
-    const response = await fetch('/api/users/');
+  const response = await fetch("/api/users/");
 
-    if (response.ok) {
-        const data = await response.json();
-        dispatch(getUsers(data))
-    }
-}
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(getUsers(data));
+    return data;
+  }
+};
 
-export const removeServerMember = (serverId, userId) => async (dispatch) => {
-    const response = await fetch(`/api/servers/${serverId}/members/${userId}`,{
-        method:"DELETE"
-        });
+export const addServerMember = (serverId, userId) => async (dispatch) => {
+  console.log(
+    serverId,
+    userId,
+    "server and user Id in thunk for adding memeber"
+  );
+  const response = await fetch(`/api/servers/${serverId}/members`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: {
+      user_id: userId,
+      server_id: serverId,
+    },
+  });
 
-    if (response.ok) {
-      const data = await response.json();
-      removeMember(userId)
-      console.log(data)
-    }
-}
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(addMember(data));
+    console.log(data);
+    return data;
+  }
+};
 
-let initialState = { allUsers: {}, serverUsers: {} }
+export const removeServerMember = (serverId, memberId) => async (dispatch) => {
+  const response = await fetch(`/api/servers/${serverId}/members/${memberId}`, {
+    method: "DELETE",
+  });
 
-export default function userReducer(state = initialState, action){
-    switch(action.type) {
-        case GET_USERS:
-            return {allUsers:{...action.users}, serverUsers:{...state.serverUsers}}
-        case REMOVE_MEMBER:
-            let newState = {...state}
-            delete state.serverUsers[action.memberId]
-            return newState
-        case SET_USERS:
-            return {allUsers:{...state.allUsers}, serverUsers:{...action.users}}
-        default:
-            return state
-    }
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(removeMember(data));
+    console.log(data);
+    return data;
+  }
+};
+
+let initialState = { allUsers: {}, serverUsers: {} };
+
+export default function userReducer(state = initialState, action) {
+  let newState;
+  switch (action.type) {
+    case GET_USERS:
+      return {
+        allUsers: { ...action.users },
+        serverUsers: { ...state.serverUsers },
+      };
+    case GET_MEMBERS:
+      return {
+        serverUsers: { ...action.users },
+        allUsers: { ...state.allUsers },
+      };
+    case ADD_MEMBER:
+      newState = { ...state };
+      if (!state.serverUsers[action.user.id]) {
+        newState.serverUsers[action.user.id] = action.user;
+      }
+      //   else {
+      //     newState.serverUsers[action.user.id] = action.user;
+      //     return newState;
+      //   }
+      return newState;
+    case REMOVE_MEMBER:
+      newState = { ...state };
+      delete newState.serverUsers[action.memberId];
+      return newState;
+    case SET_USERS:
+      return {
+        allUsers: { ...state.allUsers },
+        serverUsers: { ...action.users },
+      };
+
+    default:
+      return state;
+  }
 }
