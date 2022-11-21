91b5acc37772a2632e7b5bef94569cfc1a0beed6
ADUB <rootbeer608@gmail.com>
2022-11-21 01:15:13 -0800

    signup validations fixed

--- practice-for-week-19-python-project-skeleton/react-app/src/components/auth/SignUpForm.js
+++ practice-for-week-19-python-project-skeleton/react-app/src/components/auth/SignUpForm.js
@@ -17,8 +17,15 @@ const SignUpForm = () => {
   const dispatch = useDispatch();
   const users = useSelector((state) => state.users.allUsers);
 
-  const usernames = []
+  const usernames = [];
 
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
 
   const onSignUp = async (e) => {
     e.preventDefault();
@@ -30,21 +37,41 @@ const SignUpForm = () => {
       password,
     };
 
+    let errors = [];
 
+    if (username.length <= 4 || username.length >= 50) {
+      errors.push(
+        "Username must be more than 4 characters and less than 50 characters "
+      );
+    }
 
+    if (password.length <= 6 || password.length >= 50) {
+      errors.push(
+        "Password must be more than 6 characters and less than 50 characters "
+      );
+    }
 
-    if (password === repeatPassword) {
+    if (!validateEmail(email)) {
+      errors.push("Email must be a valid email");
+    }
+
+    if (password !== repeatPassword) {
+      errors.push("Passwords must match");
+    }
+
+    if (errors.length === 0) {
       const data = await dispatch(signUp(userData));
       if (data) {
         setErrors(data);
       }
     } else {
-      setErrors(["Passwords must match"])
+      setErrors(errors);
     }
   };
 
   const updateUsername = (e) => {
     setUsername(e.target.value);
+    console.log(username, "username");
   };
   const updateFirstName = (e) => {
     setFirstName(e.target.value);
