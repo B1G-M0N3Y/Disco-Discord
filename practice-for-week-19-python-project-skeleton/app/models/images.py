ce8c104fabccb3390f8efff57bfc1851ed42095d
B1G-M0N3Y <noahkerner843@gmail.com>
2022-12-28 13:10:01 -0700

    added minimal images.py backend model

---
+++ practice-for-week-19-python-project-skeleton/app/models/images.py
@@ -0,0 +1,8 @@
+from .db import db, environment, SCHEMA
+class Image(db.Model):
+    __tablename__ = 'images'
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    image_url = db.Column(db.String(255), nullable=False)
