e370eb9eb5b889001a5e87b505cb02f3c2a796e9
B1G-M0N3Y <noahkerner843@gmail.com>
2023-01-05 14:59:11 -0700

    now you can just boom

--- practice-for-week-19-python-project-skeleton/react-app/src/components/auth/ProfileImageSubmit.js
+++ practice-for-week-19-python-project-skeleton/react-app/src/components/auth/ProfileImageSubmit.js
@@ -12,47 +12,48 @@ const ProfileImageSubmit = ({ image, setImage, setPosition, position }) => {
 
   const ImagePreview = () => {
     if (image) return (
-      <img src={URL.createObjectURL(image)}></img>
+      <div className="sign-up-uploaded-image">
+        <img src={URL.createObjectURL(image)}></img>
+      </div>
     )
-
-    return <p>No Image to Preview</p>
+    return (
+      <div className='sign-up-file-upload'>
+        <div className="sign-up-file-upload-text">
+          <p>Click here to upload a file</p>
+          <p>or</p>
+          <p>Drag and drop from your computer</p>
+        </div>
+        <p className="sign-up-file-extensions">* extensions allowed: {ALLOWED_TYPES.map((type, idx) => {
+          if (idx === (ALLOWED_TYPES.length - 1)) {
+            return `${type}.`
+          }
+          return `${type}, `
+        }
+        )}</p>
+      </div>
+    )
   }
 
   return (
     <div className="sign-up-image-page">
-      <p id="sign up title">Upload Profile Image</p>
       <FileUploader
         label='Something Groovy'
         types={ALLOWED_TYPES}
         handleChange={handleChange}
         children={
-          <>
-            <div className='sign-up-image-preview'>
-              <ImagePreview />
-            </div>
-            <div className='sign-up-file-upload'>
-              <div className="sign-up-file-upload-text">
-                <p>Click here to upload a file</p>
-                <p>or</p>
-                <p>Drag and drop from your computer</p>
-              </div>
-              <p className="sign-up-file-extensions">* extensions allowed: {ALLOWED_TYPES.map((type, idx) => {
-                if (idx === (ALLOWED_TYPES.length - 1)) {
-                  return `${type}.`
-                }
-                return `${type}, `
-              }
-              )}</p>
-            </div>
-          </>
+          <div className="sign-up-image-upload">
+            <ImagePreview />
+          </div>
         }
       />
-      <button className="sign-up-button">Submit</button>
-      <div className="image-submit-back">
-        <p>Change Credentials</p>
-        <button className="sign-up-back" onClick={() => setPosition(position - 1)}>
-          <i class="fa-solid fa-caret-left"></i>Back
-        </button>
+      <div className="image-submit-footer">
+        <div className="image-submit-back">
+          <p>Change Credentials</p>
+          <button className="sign-up-back" onClick={() => setPosition(position - 1)}>
+            <i class="fa-solid fa-caret-left"></i>Back
+          </button>
+        </div>
+        <button className="sign-up-button">Submit</button>
       </div>
     </div>
   )
